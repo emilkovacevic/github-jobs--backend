0e38e140dec3c97007abd5177fa9f0dfcfc6bb15
Emil Kovacevic <77700903+emilkovacevic@users.noreply.github.com>
2021-04-15 23:41:43 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const axios = require('axios');
+var cors = require('cors')
+const app = express()
+const port = 5000
+
+app.use(cors())
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+  });
+  
+app.get("/", (req, res) => res.send('server running'))
+
+app.get("/jobs", (req, res) => {
+    axios.get('https://jobs.github.com/positions.json')
+	.then(response => res.send(response.data))
+	.catch(err => console.log(err))
+})
+
+app.listen(port, ()=> console.log('exampe listen port on ' + port))
